4c0714498ebb841dfa62d9ad062dd7ad0ff660f0
Marcos Fernandes <marcosdoriguetto@gmail.com>
2022-01-04 12:39:01 -0300

    Refatoração do componente ModalEditFood

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -4,40 +4,36 @@ import { FiCheckSquare } from 'react-icons/fi';
 import { Form } from './styles';
 import { Modal } from '../Modal';
 import { Input } from '../Input';
+import { DataFoodType } from '../ModalAddFood';
 
-export type DataFoodType = {
-  image: string;
-  name: string;
-  description: string;
-  price: string;
-}
-
-type ModalAddFoodProps = {
+type ModalEditFoodProps = {
   setIsOpen: () => void;
-  handleAddFood: (data: DataFoodType) => void;
+  handleUpdateFood: (data: DataFoodType) => void;
+  editingFood: DataFoodType;
   isOpen: boolean; 
 }
 
-export function ModalAddFood({ setIsOpen, handleAddFood, isOpen}: ModalAddFoodProps) {
+export function ModalEditFood({ isOpen, setIsOpen, editingFood, handleUpdateFood }: ModalEditFoodProps) {
   const formRef = useRef();
 
   const handleSubmit = async (data: DataFoodType) => {
-    handleAddFood(data);
+    handleUpdateFood(data);
     setIsOpen();
   };
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef.current} onSubmit={handleSubmit}>
-        <h1>Novo Prato</h1>
+      <Form ref={formRef.current} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
         <Input name="image" placeholder="Cole o link aqui" />
 
         <Input name="name" placeholder="Ex: Moda Italiana" />
         <Input name="price" placeholder="Ex: 19.90" />
 
         <Input name="description" placeholder="Descrição" />
-        <button type="submit" data-testid="add-food-button">
-          <p className="text">Adicionar Prato</p>
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
@@ -46,3 +42,5 @@ export function ModalAddFood({ setIsOpen, handleAddFood, isOpen}: ModalAddFoodPr
     </Modal>
   );
 };
+
+export default ModalEditFood;
